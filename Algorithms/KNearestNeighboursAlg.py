16484ad3b05973661a2b7d931a4b028cc7f20406
NicholasPaquin <napaquin@uwaterloo.ca>
2019-02-28 14:10:41 -0500

    Finishes k_nearest_neighbours algorithm on breast cancer data

--- Algorithms/KNearestNeighboursAlg.py
+++ Algorithms/KNearestNeighboursAlg.py
@@ -18,11 +18,43 @@ def k_nearest_neighbours(data, predict, k=3):
 
     votes = [i[1] for i in sorted(distances)[:k]]
     vote_result = Counter(votes).most_common(1)[0][0]
-    return vote_result
+    confidence = Counter(votes).most_common(1)[0][1] / k
+    return vote_result, confidence
+
+accuracies = []
+
+for i in range(25):
+    df = pd.read_csv('datasets/breast-cancer-wisconsin.data.txt')
+    df.replace('?', -999999, inplace=True)
+    df.drop(['id'], 1, inplace=True)
+    full_data = df.astype(float).values.tolist()
+
+    random.shuffle(full_data)
+
+    test_size = 0.2
+    train_set = {2: [], 4: []}
+    test_set = {2: [], 4: []}
+    train_data = full_data[:-int(test_size*len(full_data))]
+    test_data = full_data[-int(test_size*len(full_data)):]
+    for i in train_data:
+        train_set[i[-1]].append(i[:-1])
+
+    for i in test_data:
+        test_set[i[-1]].append(i[:-1])
 
 
-df = pd.read_csv('datasets/breast-cancer-wisconsin.data.txt')
-df.replace('?', -999999, inplace=True)
-df.drop(['id'], 1, inplace=True)
+    correct = 0
+    total = 0
 
+    for group in test_set:
+        for data in test_set[group]:
+            vote, confidence = k_nearest_neighbours(train_set, data, k=5)
+            if group == vote:
+                correct += 1
+            total += 1
+
+    print('Accuracy:', correct/total)
+    accuracies.append(correct/total)
+
+print(sum(accuracies)/len(accuracies))
 
