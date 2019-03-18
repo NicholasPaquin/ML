0ca24555d71dd94da23f742fc350de055e3649d6
NicholasPaquin <napaquin@uwaterloo.ca>
2019-03-17 22:16:16 -0400

    working on optimizing svm

---
+++ Algorithms/SVMAlg.py
@@ -0,0 +1,89 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+
+class Support_Vector_Machine:
+    def __init__(self, visualization=True):
+        self.vizualization = visualization
+        self.colors = {1: 'r', -1: 'b'}
+        if self.vizualization:
+            self.fig = plt.figure()
+            self.ax = self.fig.add_subplot(1, 1, 1)
+
+    def fit(self, data):
+        self.data = data
+
+        #{||w||: [w,b]}
+        opt_dict = {}
+
+        transforms = [[1,1],
+                      [-1,1],
+                      [-1,-1],
+                      [1,-1]]
+
+        all_data = []
+        for yi in self.data:
+            for featureset in self.data[yi]:
+                for feature in featureset:
+                    all_data.append(feature)
+
+        self.max_feature_value = max(all_data)
+        self.min_feature_value = min(all_data)
+        all_data = None
+
+        step_size = [self.max_feature_value * 0.1,
+                     self.max_feature_value * 0.01,
+                     # point of expense:
+                     self.max_feature_value * 0.001]
+
+        # very expensive
+        b_range_multiple = 5
+
+        b_multiple = 5
+
+        latest_optimum = self.max_feature_value*10
+
+        for step in step_size:
+            w = np.array([latest_optimum, latest_optimum])
+
+            #this is possible as it is convex
+            optimized = False
+
+            while not optimized:
+                for b in np.arange(-1 * (self.max_feature_value * b_range_multiple),
+                                   self.max_feature_value * b_range_multiple,
+                                   step * b_multiple):
+                    for traformation in transforms:
+                        w_t = w * traformation
+                        found_option = True
+
+                        #weakest part of svm
+                        #yi(xi.w+b) >= 1
+                        for i in self.data:
+                            for xi in self.data[i]:
+                                yi = i
+                                if not yi * (np.dot(w_t,xi) + b):
+                                    found_option = False
+
+                        if found_option:
+                            opt_dict[np.linalg.norm(w_t)] = [w_t, b]
+
+                if w[0] < 0:
+                    optimized = True
+                    print('Optimized a step.')
+                else: w = w - step
+
+    def predict(self, features):
+        classification = np.sign(np.dot(np.array(features), self.w) + self.b)
+
+
+data_dict = {-1: np.array([[1, 7],
+                           [2, 8],
+                           [3, 8]]),
+             1: np.array([[5, 1],
+                        [6, -1],
+                        [7, 3]])}
+
+
+
